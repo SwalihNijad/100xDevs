6325eee468b77541eba738b03a4f2297debebba6
Swalih Nijad <swalihnijad123@gmail.com>
2026-02-22 23:20:05 +0530

    function even/odd

---
+++ 2 JS/Assignment3.js
@@ -0,0 +1,14 @@
+function IsEven(num){
+    if (num % 2 == 0){
+        console.log("The number is Even")
+    } else {
+        console.log("The number is odd")
+    }
+}
+
+IsEven(10)
+IsEven(67)
+IsEven(23)
+IsEven(40)
+
+
