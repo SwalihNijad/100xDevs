4386fa1dd4548d91db92aef3d13032536b678529
Swalih Nijad <swalihnijad123@gmail.com>
2026-02-27 23:30:54 +0530

    Synchronous/Asynchronous

--- CLASS 3/index.js
+++ CLASS 3/index.js
@@ -4,6 +4,26 @@ const contents = fs.readFileSync("a.txt","utf-8");
 console.log (contents);
 
 for  (let i=0; i<100 ;i++ );
-{
+{                                  
     
+}     
+//here how js will move from reading file to any looop!! (line16)
+
+//synchronous - after one completed it will move to another (waiting near washing machine to complete)
+
+//Asynchronous - When one is already running it will move to another (putting clothes to washing machine to complete and doing other works)
+
+//So for that we have to write
+
+function FileReadCallback (err, contents){
+    console.log(contents);
 }
+
+fs.readFile("a,txt","utf-8",FileReadCallback);
+
+let s =0
+for (let i = 0 ; i<1000000 ; i++);{
+    s += i
+}
+
+console.log(s)
