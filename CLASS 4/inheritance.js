b3e450a82f53100f66f0420685f784c8f1a84450
Swalih Nijad <swalihnijad123@gmail.com>
2026-03-10 00:01:21 +0530

    Inheritance

--- CLASS 4/inheritance.js
+++ CLASS 4/inheritance.js
@@ -4,7 +4,7 @@ class shape{
     }
 
     paint() {
-        console.log("Painting with the color" + this.color)
+        console.log("Painting with the color: " + this.color)
     }
 }
 
@@ -58,8 +58,9 @@ class Square extends shape {
     }
 }
 
-const r1 = new Rectangle(10, 10, "red")
+const r1 = new Rectangle(10, 10, "Orange")
 const c1 = new Circle(10 , "Black")
-const s1 = new Square(300, "red")
+const s1 = new Square(300, "Red")
 console.log(c1.paint())
 console.log(s1.paint())
+console.log(r1.paint())
