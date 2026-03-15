a5668710434696181888107e2b32980a28ed4cc7
Swalih Nijad <swalihnijad123@gmail.com>
2026-03-15 22:31:01 +0530

    Writing promisified function

--- CLASS 4/promisified.js
+++ CLASS 4/promisified.js
@@ -1,6 +1,6 @@
-//Create a promisisfied of fs.readfile
-//Create a promisisfied of setTimeout
-//Create a promisisfied of fs.writefile
+//(i)Create a promisisfied of fs.readfile
+//(ii)Create a promisisfied of setTimeout
+//(iii)Create a promisisfied of fs.writefile
 
 const fs = require("fs");
 
@@ -29,11 +29,21 @@ fsReadFilePromise("a.txt","utf-8")
     })
 
 
-
-
-
-
-
-
-
 fsReadFilePromise("a.txt","utf-8")
+
+//Callback has two arguments followed by number where as promisified callback has never passed 1 arg will be reduced  i.e setTimeout(function(){}1000) and seTimeout(1000)and .then and .catch
+
+//(ii)
+
+function setTimeoutpromisified(delay){
+    return new Promise (function(resolve, reject){   //promise should resolve after 1 sec
+        setTimeout(function(){
+            resolve()
+        },delay)
+    })
+}
+
+setTimeoutpromisified(1000)
+    .then (function(){
+        console.log("1 second has delayed")
+    })
