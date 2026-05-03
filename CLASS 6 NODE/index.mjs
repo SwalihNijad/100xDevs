055b1e2d58e4c7f06245aeb2fbbca559036604d7
Swalih Nijad <swalihnijad123@gmail.com>
2026-05-03 20:18:38 +0530

    node

---
+++ CLASS 6 NODE/index.mjs
@@ -0,0 +1,7 @@
+import chalk from "chalk";      //Modern inport syntax, promises
+
+console.log(chalk.blue('Hello World!'));          
+console.log(chalk.red('This is an error message'));
+console.log(chalk.green('This is a success message.'));
+
+//node --experimental-modules index.mjs   (to run in terminal)
