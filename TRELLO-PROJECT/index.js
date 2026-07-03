74bd724e5c7a25d8d9556dcad1f5a734d1f32b67
Swalih Nijad <swalihnijad123@gmail.com>
2026-07-03 10:24:27 +0530

    backend

--- TRELLO-PROJECT/index.js
+++ TRELLO-PROJECT/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require(jsonwebtoken);
-const { authmiddleware } = require("./middleware");
+const { authMiddleware } = require("./middleware");
 
 let USERS_ID = 1;
 let ORGANIZATION = 1;
@@ -34,12 +34,56 @@ app.post("./signup",(req, res) => {
 
     USERS.push({
         username,
-        passswrod,
+        passsword,
         id: USERS_ID++
     })
 
     res.json({
         message: "You have signed up successfully"
     })
+})
+
+app.post("./signin",(req,res) => {
+    const usesrname = req.body.username;
+    const password = req.body.passsword;
+
+    const userExists = USERS.find(u => u.username == username && u.passsword)
+
+    if(!userExists){
+        req.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
+
+    const token = jwt.sign({
+        userID: userExists.id
+    }, "attlasiationsupersecret123123password")
+})
+
+//AUTHENTICATED MIDDLEWARE
+app.post("/organization",authMiddleware,(req,res) => {
+    const userID = req.userID;
+    ORGANIZATION.push({
+        id: ORGANIZATION_ID++,
+        title: req.body.title,
+        description:req.body.description,
+        admin: userID, 
+        members: []
+    })
+})
+
+app.post("/add-member-to-organization", authMiddleware, (req, res) => {
+    const userID = req.userID;
+    const organizationID = req.body.organizationID;
+    const memerUserUsername = req.body.memerUserUsername;
+
+    const organization = ORGANIZATION.find(org => org.id === organizationID);
+    
+     if (!organization || organization.admin !== userId) {
+        res.status(411).json({
+            message: "Either this org doesnt exist or you are not an admin of this org"
+        })
+        return
+    }
 })
 
