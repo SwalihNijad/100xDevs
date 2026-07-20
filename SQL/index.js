9d4fe5c00433e4a16bc36f227fb273557f81213d
Swalih Nijad <swalihnijad123@gmail.com>
2026-07-20 22:11:08 +0530

    Backend

---
+++ SQL/index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const { pool } = require('pg')
+
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_QasvduKy9pf3@ep-raspy-wind-awuf0ip2-pooler.c-12.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+const app = express();
+app.use(express.json())
+
+app.post("/signup", async(req,res) => {
+    const username = req.body.username
+    const email = req.body.email
+    const password = req.body.password
+
+    const response = await pool.query(`INSERT INTO users(username, email, password) VALUES ($1, $2, $3)RETURNING id;`,[username, email, password])
+    
+    res.json({
+        message: "Signup done!"
+        //id: response.rows[0].id
+    })
+})
+
+app.post("/signin", async(req, res) => {
+    const username = req.body.username
+    const email = req.body.email
+
+    const response = await pool.query(`SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`);
+
+    const userExists = response.rows[0];
+
+    if(!userExists) {
+        res.status(403).json({
+            message: "incorrect creds"
+        })
+    } else {
+        res.jsonp({
+            token: "erfyueoihjfjkelchihdsfcivivdc"
+        })
+    }
+})
+
+app.listen(3000)
